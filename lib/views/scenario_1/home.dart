fa989ec355a79b7751ec2a8095362bf64727859d
Sai Sampath Kumar <balivadask2000@gmail.com>
2020-06-29 16:56:56 +0530

    completed scenario 1

--- lib/views/scenario_1/home.dart
+++ lib/views/scenario_1/home.dart
@@ -13,32 +13,35 @@ class _ScenarioOneState extends State<ScenarioOne> {
   bool _isThinking = false;
   int presentChat = 1;
   double opacity = 0;
+  final _controller = ScrollController();
+
+  void increment() {
+    setState(() {
+      if (presentChat < chat.length) {
+        presentChat++;
+      }
+      print(presentChat);
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      if (presentChat > 0) {
+        presentChat--;
+      }
+      print(presentChat);
+    });
+  }
+
+  void thinkChanger() {
+    setState(() {
+      _isThinking = !_isThinking;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    void increment() {
-      setState(() {
-        if (presentChat < chat.length) {
-          presentChat++;
-        }
-        print(presentChat);
-      });
-    }
-
-    void decrement() {
-      setState(() {
-        if (presentChat > 0) {
-          presentChat--;
-        }
-        print(presentChat);
-      });
-    }
-
-    void thinkChanger() {
-      setState(() {
-        _isThinking = !_isThinking;
-      });
-    }
+    final _height = MediaQuery.of(context).size.height;
 
     return Scaffold(
       backgroundColor: Colors.black,
@@ -56,35 +59,41 @@ class _ScenarioOneState extends State<ScenarioOne> {
           ),
           Hero(
             tag: 'scenario1',
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: presentChat,
-              itemBuilder: (content, index) {
-                return Opacity(
-                  opacity:
-                      (index < presentChat - 1 || chat[index][1] == 'assist')
-                          ? 1.0
-                          : opacity,
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 4, bottom: 4),
-                    child: Align(
-                      alignment: (chat[index][1] == 'assist')
-                          ? Alignment.centerLeft
-                          : Alignment.centerRight,
-                      child: MessageContainer(
-                        thinkingChanger: thinkChanger,
-                        increment: increment,
-                        decrement: decrement,
-                        inputMessage: chat[index][0],
-                        type: chat[index][1],
-                        color: Color(
-                          int.parse(chat[index][2]),
+            child: Container(
+              height: _height * 0.9,
+              child: ListView.builder(
+                controller: _controller,
+                shrinkWrap: true,
+                itemCount: presentChat,
+                itemBuilder: (content, index) {
+                  _controller.jumpTo(_controller.position.maxScrollExtent);
+
+                  return Opacity(
+                    opacity:
+                        (index < presentChat - 1 || chat[index][1] == 'assist')
+                            ? 1.0
+                            : opacity,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 4, bottom: 4),
+                      child: Align(
+                        alignment: (chat[index][1] == 'assist')
+                            ? Alignment.centerLeft
+                            : Alignment.centerRight,
+                        child: MessageContainer(
+                          thinkingChanger: thinkChanger,
+                          increment: increment,
+                          decrement: decrement,
+                          inputMessage: chat[index][0],
+                          type: chat[index][1],
+                          color: Color(
+                            int.parse(chat[index][2]),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
           ),
         ],
